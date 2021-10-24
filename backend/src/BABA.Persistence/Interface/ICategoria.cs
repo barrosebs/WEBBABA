c5e8f7f840567439c4f5bf61b15bebac5efc691f
Eduardo Barros <barrosebs@gmail.com>
2021-10-24 12:46:36 -0300

    Adicionando Interface e correção no documento de Diagrama de Contexto

---
+++ backend/src/BABA.Persistence/Interface/ICategoria.cs
@@ -0,0 +1,16 @@
+﻿using BABA.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BABA.Persistence.Interface
+{
+    public interface ICategoria
+    {
+        Task<Categoria[]> GetAllCategoriaByAtletaAsync(string nome, bool includeAtleta= false);
+        Task<Categoria[]> GetAllCategoriaAsync();
+        Task<Categoria> GetCategoriaByIdAsync(int categoriaId, bool includeAtleta = false);
+    }
+}
---
+++ backend/src/BABA.Persistence/Interface/IMensalidade.cs
@@ -0,0 +1,12 @@
+﻿using BABA.Domain.Models;
+using System.Threading.Tasks;
+
+namespace BABA.Persistence.Interface
+{
+    public interface IMensalidade
+    {
+        Task<Mensalidade[]> GetAllMensalidadeByAtletaAsync(string nome, bool includeAtleta = false);
+        Task<Mensalidade[]> GetAllMensalidadeAsync();
+        Task<Mensalidade> GetMensalidadeByIdAsync(int mensalidadeId, bool includeAtleta = false);
+    }
+}
