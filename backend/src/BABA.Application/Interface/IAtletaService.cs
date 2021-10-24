c8a8046f8a59d187bf78c09dc100b4f5dc92f6a4
Eduardo Barros <barrosebs@gmail.com>
2021-10-24 16:24:18 -0300

    Adicionado Applications Atletla e Services de Atleta, Controle e Usuario

---
+++ backend/src/BABA.Application/Interface/IAtletaService.cs
@@ -0,0 +1,20 @@
+﻿using BABA.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BABA.Application.Interface
+{
+    public interface IAtletaService
+    {
+        Task<Atleta> AddAtleta(Atleta model);
+        Task<Atleta> UpdateAtleta(int atletaId, Atleta model);
+        Task<bool> DeleteAtleta(int atletaId);
+
+        Task<Atleta[]> GetAllAtletaByAtletaAsync(string condutor, bool includeMensalidade = false);
+        Task<Atleta[]> GetAllAtletaAsync(bool includeAtleta = false);
+        Task<Atleta> GetAtletaByIdAsync(int atletaId, bool includeMensalidade = false);
+    }
+}
---
+++ backend/src/BABA.Application/Interface/IControleService.cs
@@ -0,0 +1,20 @@
+﻿using BABA.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BABA.Application.Interface
+{
+    public interface IControleService
+    {
+        Task<Controle> AddControle(Controle model);
+        Task<Controle> UpdateControle(int controleId, Controle model);
+        Task<bool> DeleteControle(int controleId);
+
+        Task<Controle[]> GetAllControleByAtletaAsync(string condutor, bool includeAtleta = false);
+        Task<Controle[]> GetAllControleAsync(bool includeAtleta = false);
+        Task<Controle> GetControleByIdAsync(int controleId, bool includeAtleta = false);
+    }
+}
