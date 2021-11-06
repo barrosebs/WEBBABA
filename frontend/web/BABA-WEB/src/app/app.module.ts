72e6e953719495ffe6053344ba57d931fb3bdac1
Eduardo Barros <barrosebs@gmail.com>
2021-11-06 14:22:48 -0300

    ajuste na Startup para correção do CORS acoplamento do front para componente atleta

--- frontend/web/BABA-WEB/src/app/app.module.ts
+++ frontend/web/BABA-WEB/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AtletasComponent } from './atletas/atletas.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      AtletasComponent,
+      MenuComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
