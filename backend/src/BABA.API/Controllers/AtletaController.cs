7b27912b0c9e9878da58b88fbeb1503f6cb49366
Eduardo Barros <eduardo.santos@acbprocessamento.com.br>
2021-11-11 16:38:41 -0300

    Ajustes na controller atleta

--- backend/src/BABA.API/Controllers/AtletaController.cs
+++ backend/src/BABA.API/Controllers/AtletaController.cs
@@ -3,8 +3,6 @@ using BABA.Domain.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
