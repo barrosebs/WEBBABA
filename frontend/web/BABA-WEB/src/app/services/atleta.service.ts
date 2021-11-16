ca0de4e7a3dce4f587588d80153f0a83704cff38
Eduardo Barros <eduardo.santos@acbprocessamento.com.br>
2021-11-16 17:32:18 -0300

    criando o service de atleta

--- frontend/web/BABA-WEB/src/app/services/atleta.service.ts
+++ frontend/web/BABA-WEB/src/app/services/atleta.service.ts
@@ -3,13 +3,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Atleta } from '../models/atleta';
 
-  @Injectable({
+  @Injectable({ //chamada direto do app.module/providers
     providedIn: 'root'
   })
 
 export class AtletaService {
 
-  baseUrl = 'https://localhost:5001/api/atleta';
+  baseUrl = 'https://localhost:5001/api/atletaS';
 
 constructor(private http: HttpClient) { }
 
@@ -17,9 +17,9 @@ getAllAtleta(): Observable<Atleta[]>{
     return this.http.get<Atleta[]>(this.baseUrl);
   }
   getAtletaByTema(tema: string): Observable<Atleta[]>{
-    return this.http.get<Atleta[]>('$ {this.baseUrl}/getByTema/${tema} ');
+    return this.http.get<Atleta[]>('${this.baseUrl}/getByTema/${tema}/tema');
   }
-  getAtletaById(id: number): Observable<Atleta[]>{
-    return this.http.get<Atleta[]>('$ {this.baseUrl}/${id} ');
+  getAtletaById(id: number): Observable<Atleta>{
+    return this.http.get<Atleta>('${this.baseUrl}/${id} ');
   }
 }
