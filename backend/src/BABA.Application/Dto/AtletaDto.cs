ca454e93efa0dd336d113142c7e0ef7b0a518b1a
Eduardo Barros <eduardo.santos@acbprocessamento.com.br>
2021-12-02 11:42:49 -0300

    Especificando alguns Data Annotations class Atleta

--- backend/src/BABA.Application/Dto/AtletaDto.cs
+++ backend/src/BABA.Application/Dto/AtletaDto.cs
@@ -7,18 +7,24 @@ namespace BABA.Application.Dto
     public class AtletaDto
     {
         public int AtletaId { get; set; }
+        [Display(Name = "Nome do Atleta")]
         [MinLength(10, ErrorMessage = "Campo {0} é obirgatório mínimo de 3")]
         [Required(ErrorMessage = "Campo {0} é obrigatório")]
         public string Nome { get; set; }
         public string Apelido { get; set; }
         [Required(ErrorMessage = "Campo {0} é obrigatório")]
         public int Camisa { get; set; }
+        [Display(Name = "Posição do Atleta")]
         public int Posicao { get; set; }
         public bool Comissao { get; set; }
         [Required(ErrorMessage = "Campo {0} é obrigatório")]
         public string WhatsApp { get; set; }
+        [Display(Name = "Data de Nascimento")]
         [Required(ErrorMessage = "Campo {0} é obrigatório")]
         public DateTime DataNascimento { get; set; }
+        [Display(Name = "Foto do Atleta")]
+        [RegularExpression(@".*\.(jpe?g|bmp|png)$",
+                            ErrorMessage = "Formato não permiti!")]
         public virtual string imageUrl { get; set; }
         public List<MensalidadeDto> Mensalidades { get; set; }
     }
