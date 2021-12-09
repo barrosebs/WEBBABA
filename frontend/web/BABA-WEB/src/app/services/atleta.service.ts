85fbc5410748e0c4eddd29f9cb9a4c7609048d0c
Eduardo Barros <eduardo.santos@acbprocessamento.com.br>
2021-12-09 11:54:51 -0300

    desescrevendo o subscrebilie

--- frontend/web/BABA-WEB/src/app/services/atleta.service.ts
+++ frontend/web/BABA-WEB/src/app/services/atleta.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Atleta } from '../models/atleta';
-
+import { take } from 'rxjs/operators';
   @Injectable({ //chamada direto do app.module/providers
     providedIn: 'root'
   })
@@ -14,23 +14,33 @@ export class AtletaService {
 constructor(private http: HttpClient) { }
 
 public getAllAtleta(): Observable<Atleta[]>{
-    return this.http.get<Atleta[]>(this.baseUrl);
+    return this.http
+    .get<Atleta[]>(this.baseUrl)
+    .pipe(take(1));
   }
 public getAtletaByTema(tema: string): Observable<Atleta[]>{
-    return this.http.get<Atleta[]>(`${this.baseUrl}/getByTema/${tema}/tema`);
+    return this.http
+    .get<Atleta[]>(`${this.baseUrl}/getByTema/${tema}/tema`)
+    .pipe(take(1));
   }
 public  getAtletaById(id: number): Observable<Atleta>{
     return this.http.get<Atleta>(`${this.baseUrl}/${id}`);
   }
   public post(atleta: Atleta): Observable<Atleta>{
-    return this.http.post<Atleta>(this.baseUrl, atleta);
+    return this.http
+    .post<Atleta>(this.baseUrl, atleta)
+    .pipe(take(1));
   }
 
  public put(atleta: Atleta): Observable<Atleta>{
-    return this.http.put<Atleta>(`${this.baseUrl}/${atleta.atletaId}`, atleta);
+    return this.http
+    .put<Atleta>(`${this.baseUrl}/${atleta.atletaId}`, atleta)
+    .pipe(take(1));
   }
 
  public deleteAtleta(id: number): Observable<boolean>{
-    return this.http.delete<boolean>(`${this.baseUrl}/${id}`);
+    return this.http
+    .delete<boolean>(`${this.baseUrl}/${id}`)
+    .pipe(take(1));
   }
 }
